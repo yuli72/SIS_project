e8452b6a0f9a0342f0a5edf03c61d787045cd23a
Yuli <yuliaballe@gmail.com>
2024-03-08 11:10:04 +0800

    commit 1

--- StudentInfoSystem/frmLogin.Designer.cs
+++ StudentInfoSystem/frmLogin.Designer.cs
@@ -56,9 +56,10 @@
             this.panel1.Controls.Add(this.label3);
             this.panel1.Controls.Add(this.button4);
             this.panel1.Dock = System.Windows.Forms.DockStyle.Bottom;
-            this.panel1.Location = new System.Drawing.Point(0, 421);
+            this.panel1.Location = new System.Drawing.Point(0, 518);
+            this.panel1.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.panel1.Name = "panel1";
-            this.panel1.Size = new System.Drawing.Size(887, 72);
+            this.panel1.Size = new System.Drawing.Size(887, 89);
             this.panel1.TabIndex = 6;
             // 
             // label4
@@ -66,7 +67,7 @@
             this.label4.AutoSize = true;
             this.label4.Font = new System.Drawing.Font("Century Gothic", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label4.ForeColor = System.Drawing.Color.White;
-            this.label4.Location = new System.Drawing.Point(12, 48);
+            this.label4.Location = new System.Drawing.Point(12, 59);
             this.label4.Name = "label4";
             this.label4.Size = new System.Drawing.Size(75, 15);
             this.label4.TabIndex = 17;
@@ -77,7 +78,7 @@
             this.label3.AutoSize = true;
             this.label3.Font = new System.Drawing.Font("Century Gothic", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label3.ForeColor = System.Drawing.Color.White;
-            this.label3.Location = new System.Drawing.Point(12, 26);
+            this.label3.Location = new System.Drawing.Point(12, 32);
             this.label3.Name = "label3";
             this.label3.Size = new System.Drawing.Size(105, 16);
             this.label3.TabIndex = 16;
@@ -90,8 +91,9 @@
             this.button4.Font = new System.Drawing.Font("Century Gothic", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.button4.ForeColor = System.Drawing.Color.White;
             this.button4.Location = new System.Drawing.Point(0, 0);
+            this.button4.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.button4.Name = "button4";
-            this.button4.Size = new System.Drawing.Size(224, 32);
+            this.button4.Size = new System.Drawing.Size(224, 39);
             this.button4.TabIndex = 15;
             this.button4.Text = "www.benedictocollege.edu.ph";
             this.button4.UseVisualStyleBackColor = true;
@@ -102,8 +104,9 @@
             this.panel2.Controls.Add(this.button3);
             this.panel2.Dock = System.Windows.Forms.DockStyle.Top;
             this.panel2.Location = new System.Drawing.Point(0, 0);
+            this.panel2.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.panel2.Name = "panel2";
-            this.panel2.Size = new System.Drawing.Size(887, 60);
+            this.panel2.Size = new System.Drawing.Size(887, 74);
             this.panel2.TabIndex = 7;
             // 
             // button3
@@ -113,9 +116,10 @@
             this.button3.Font = new System.Drawing.Font("Century Gothic", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.button3.ForeColor = System.Drawing.Color.White;
             this.button3.Image = ((System.Drawing.Image)(resources.GetObject("button3.Image")));
-            this.button3.Location = new System.Drawing.Point(839, 12);
+            this.button3.Location = new System.Drawing.Point(839, 15);
+            this.button3.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.button3.Name = "button3";
-            this.button3.Size = new System.Drawing.Size(34, 32);
+            this.button3.Size = new System.Drawing.Size(34, 39);
             this.button3.TabIndex = 14;
             this.button3.UseVisualStyleBackColor = true;
             // 
@@ -125,7 +129,7 @@
             this.lbl_benedicto.BackColor = System.Drawing.Color.Transparent;
             this.lbl_benedicto.Font = new System.Drawing.Font("Georgia", 20.25F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.lbl_benedicto.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
-            this.lbl_benedicto.Location = new System.Drawing.Point(98, 82);
+            this.lbl_benedicto.Location = new System.Drawing.Point(98, 101);
             this.lbl_benedicto.Name = "lbl_benedicto";
             this.lbl_benedicto.Size = new System.Drawing.Size(250, 31);
             this.lbl_benedicto.TabIndex = 9;
@@ -137,7 +141,7 @@
             this.lbl_cebu.BackColor = System.Drawing.Color.Transparent;
             this.lbl_cebu.Font = new System.Drawing.Font("Georgia", 14.25F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.lbl_cebu.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(24)))), ((int)(((byte)(27)))), ((int)(((byte)(32)))));
-            this.lbl_cebu.Location = new System.Drawing.Point(127, 124);
+            this.lbl_cebu.Location = new System.Drawing.Point(127, 153);
             this.lbl_cebu.Name = "lbl_cebu";
             this.lbl_cebu.Size = new System.Drawing.Size(192, 23);
             this.lbl_cebu.TabIndex = 13;
@@ -147,9 +151,10 @@
             // 
             this.pictureBox1.BackColor = System.Drawing.Color.Transparent;
             this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
-            this.pictureBox1.Location = new System.Drawing.Point(0, 53);
+            this.pictureBox1.Location = new System.Drawing.Point(0, 65);
+            this.pictureBox1.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(493, 371);
+            this.pictureBox1.Size = new System.Drawing.Size(493, 457);
             this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
             this.pictureBox1.TabIndex = 5;
             this.pictureBox1.TabStop = false;
@@ -159,9 +164,10 @@
             this.button2.Font = new System.Drawing.Font("Arial Narrow", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.button2.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(24)))), ((int)(((byte)(27)))), ((int)(((byte)(32)))));
             this.button2.Image = ((System.Drawing.Image)(resources.GetObject("button2.Image")));
-            this.button2.Location = new System.Drawing.Point(694, 293);
+            this.button2.Location = new System.Drawing.Point(694, 361);
+            this.button2.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(151, 45);
+            this.button2.Size = new System.Drawing.Size(151, 55);
             this.button2.TabIndex = 28;
             this.button2.Text = "     Admin";
             this.button2.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageBeforeText;
@@ -172,9 +178,10 @@
             this.btn_staff.Font = new System.Drawing.Font("Arial Narrow", 9.75F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.btn_staff.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(24)))), ((int)(((byte)(27)))), ((int)(((byte)(32)))));
             this.btn_staff.Image = ((System.Drawing.Image)(resources.GetObject("btn_staff.Image")));
-            this.btn_staff.Location = new System.Drawing.Point(526, 293);
+            this.btn_staff.Location = new System.Drawing.Point(526, 361);
+            this.btn_staff.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.btn_staff.Name = "btn_staff";
-            this.btn_staff.Size = new System.Drawing.Size(151, 45);
+            this.btn_staff.Size = new System.Drawing.Size(151, 55);
             this.btn_staff.TabIndex = 27;
             this.btn_staff.Text = "      Staff";
             this.btn_staff.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageBeforeText;
@@ -185,7 +192,7 @@
             this.label2.AutoSize = true;
             this.label2.Font = new System.Drawing.Font("Century Gothic", 8.25F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label2.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(38)))), ((int)(((byte)(42)))), ((int)(((byte)(50)))));
-            this.label2.Location = new System.Drawing.Point(497, 232);
+            this.label2.Location = new System.Drawing.Point(509, 281);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(374, 15);
             this.label2.TabIndex = 26;
@@ -196,7 +203,7 @@
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("Century Gothic", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label1.ForeColor = System.Drawing.Color.Black;
-            this.label1.Location = new System.Drawing.Point(596, 198);
+            this.label1.Location = new System.Drawing.Point(608, 239);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(176, 25);
             this.label1.TabIndex = 25;
@@ -205,18 +212,19 @@
             // pictureBox2
             // 
             this.pictureBox2.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox2.Image")));
-            this.pictureBox2.Location = new System.Drawing.Point(650, 82);
+            this.pictureBox2.Location = new System.Drawing.Point(653, 101);
+            this.pictureBox2.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.pictureBox2.Name = "pictureBox2";
-            this.pictureBox2.Size = new System.Drawing.Size(69, 73);
+            this.pictureBox2.Size = new System.Drawing.Size(69, 75);
             this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
             this.pictureBox2.TabIndex = 24;
             this.pictureBox2.TabStop = false;
             // 
             // frmLogin
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(887, 493);
+            this.ClientSize = new System.Drawing.Size(887, 607);
             this.Controls.Add(this.button2);
             this.Controls.Add(this.btn_staff);
             this.Controls.Add(this.label2);
@@ -228,6 +236,7 @@
             this.Controls.Add(this.panel1);
             this.Controls.Add(this.pictureBox1);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.Name = "frmLogin";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "frmLogin";
